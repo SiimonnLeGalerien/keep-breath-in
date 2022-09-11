8d83734e1c6709cdae992e95ccf10af3699f6c06
SiimonnLeGalerien <SiimonnLeGalerien@github.com>
2022-09-11 15:27:21 +0200

    tests des animations

---
+++ js/script.js
@@ -0,0 +1,7 @@
+console.log("PATAAAAATAAAAAAA !!!!!");
+function breathLoop () {
+    console.log("breathLoop");
+    const inspire = document.createElement('div');
+    inspire.className = 'inspire';
+    document.body.appendChild(inspire);
+}
